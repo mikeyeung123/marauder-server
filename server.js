5d786ba76a1d6940c3120935acb52aca7caaa0e0
Mike Yeung <mikeyeung123@gmail.com>
2015-04-14 20:48:15 -0400

    Added boilerplate code to server.js

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-var HTTP = require("http"),
-    express = require("express"),
+var HTTP = require("http")
+
+var express = require("express"),
     bodyParser = require("body-parser"),
     validator = require("validator"),
-    mongoURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URI ||
+    app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+var mongoURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URI ||
                "mongodb://ds061721.mongolab.com:61721/heroku_app35879167",
-    mongoose = require("mongoose");
+    mongoose = require("mongoose")
+
+mongoose.connect(mongoURI)
+var db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", function(callback) {
+    console.log("Opened mongoose db")
+})
+
+var locationSchema = mongoose.Schema({
+    login: String,
+    lat: Number,
+    lng: Number,
+    created_at: Date
+})
+
+var Location = mongoose.model("Location", locationSchema)
+
+alert("Done")
